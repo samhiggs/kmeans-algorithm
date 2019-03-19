b2cc305a848d649a4b55e472a5ce56f4e0dedf3e
samhiggs <samhiggs@outlook.com>
2019-03-19 11:05:04 +0100

    added to template and created a todo list and possible approach to task in README

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # sdm_kmeans
 Scientific Data Management Assignment 1 KMeans Clustering
+
+## Way of approaching tasks
+1. Everyone writes someone elses test cases
+2. Everyone hardcodes expected outputs of their domain
+3. Everyone takes the inputs and works on developing an output testing against test caes
+4. Split the algorithm into the 2 models and 2 teams.
+5. Train the models and tweak algorithm
+6. Test and improve
+7. Run against test set
+8. Upload
+
+## Team TODO List
+1. Write testcases
+2. Import data from txt and csv filetype into a pandas dataframe
+3. Clean data if necessary?
+4. display a summary of data and if k_clusters isn't defined then find optimal number for K (might not be necessary?)
+5. Split data into training and test set potentially creating a case for time series
+but can probably just leave that for now.
+6. Initialise clusters creating the correct data structure for them and setting up the data for parallellisation if necessary?
+7. Determine the initial observations.
+8. Create our first recursive observation algorithm.
+9. Print our result and a visual representation of it.
+
+
+## TO FIND OUT
+1. Will K always be given
+2. Is speed important, should we be concerned with parallelisation? 
+3. 
+
--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,19 @@
 '''
 #### Kmeans implementation assignment 1 for Scientific Data Management ####
 ### See test folder for tests
+### see data folder for datasets
+
+DATA SET SOURCES:
+skin_noskin: 
+description: https://archive.ics.uci.edu/ml/datasets/skin+segmentation
+data: https://archive.ics.uci.edu/ml/machine-learning-databases/00372/
+
+HTRU2: 
+description: https://archive.ics.uci.edu/ml/datasets/HTRU2
+data:https://archive.ics.uci.edu/ml/machine-learning-databases/00372/
+
 '''
+
 import numpy as np #useful for data analysis
 import pandas as pd #useful for importing files and handling dataframes.
 import sklearn as skl #useful for initial analysis
@@ -11,26 +23,56 @@ from matplotlib import plt #useful for visualising data
 
 class KMeans:
     
-    def __init__(self, filename, k_clusters):
+    def __init__(self, filename, k_clusters=0):
         self.filename = filename
         self.k_clusters = k_clusters
         self.clusters = []
         self.data = None
+        self.training_set = None
+        self.test_set = None
 
     #imports data and checks that it is a valid filetype
     def importData(self):
-        self.data = pd.read_csv("{}".format(self.filename))
+        #read the filetype and run relevant case
+        ftype = ''
+        if ftype is 'csv':
+            self.data = pd.read_csv("{}".format(self.filename))
+        if ftype is 'txt':
+            #TODO
+            pass
+        else:
+            print('{} is an invalid filetype'.format(ftype))
+            return
 
     #summary of data
     def dataSummary(self):
         #TODO
         if self.data is None or len(self.data) is 0:
             print('the data has not been created in dataSummary()')
-            return
+            return False
         print('{}\n {}\n {}\n {}\n'.format(self.data.head(), self.data.info(), self.data.describe(), self.data.columns))
+        if self.k_clusters == 0:
+            #TODO
+            pass
+
+    #If the number of clusters needs to be updated.
+    def changeClusters(self, kClusters):
+        if kClusters <= 0 or isinstance(kClusters, int) is False:
+            print('must be a positive integer > 0')
+        self.k_clusters = kClusters
+        return self.k_clusters
+    
+    #create a training and test set of the data. Timeseries will need to be handled
+    #differently to other data..
+    def create_training_test_set(self, ratio=.8, timeseries=False):
+        if ratio < 0 or ratio > 1:
+            print('ratio must be as a decimal between 0 and 1')
+            return False
+        #TODO
+        pass
 
     #assign k clusters to list
-    def initalise_clusters(self):
+    def initialise_clusters(self):
         #TODO
         pass
 
@@ -49,4 +91,15 @@ class KMeans:
     def visualise_clusters(self):
         #TODO
         pass
-    
+
+#If we want to run as a script using some test data
+if __name__ == '__main__':
+    kmeans = KMeans('data\Skin_NonSkin.txt')
+    kmeans.importData()
+    kmeans.dataSummary()
+    kmeans.initialise_clusters()
+    kmeans.initial_observations()
+    kmeans.recursive_observations()
+    kmeans.print_clusters()
+    kmeans.visualise_clusters()
+        
