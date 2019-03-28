560d6e577718ec0883d7b1991385e6e6803e785e
samhiggs <samhiggs@outlook.com>
2019-03-28 13:34:50 +0100

    macqueens working mostly

--- mq_run_script.py
+++ mq_run_script.py
@@ -16,10 +16,27 @@ def setup_csv():
     kmeans.convert_data()
     return kmeans
 
-print('running mq update')
-kmeans=setup_csv()
+kmeans=setup_txt()
+kmeans.transform_skin_noskin_data()
 init_strategy = RandomInit()
-kmeans.init_centroids = init_strategy.init(k_clusters=3, point_cloud=kmeans.point_cloud)
-kmeans.update_strategy = MacQueenUpdate()
-print('test macqueens about to start clustering')
-kmeans.optimized_clusters = kmeans.update_strategy.update(kmeans.init_centroids, kmeans.point_cloud)
+kmeans.init_centroids = init_strategy.init(k_clusters=2, point_cloud=kmeans.transformed_point_cloud)
+
+def macqueen():
+    kmeans.update_strategy = MacQueenUpdate()
+    kmeans.optimized_clusters = kmeans.update_strategy.update(kmeans.init_centroids, kmeans.transformed_point_cloud)   
+    kmeans.visualize_clusters_skin_noskin()
+
+def lloyd():
+    kmeans.update_strategy = LloydUpdate()
+    # kmeans.optimized_clusters = kmeans.update_strategy.update(kmeans.init_centroids, kmeans.point_cloud)
+    kmeans.update_strategy.update(kmeans.init_centroids, kmeans.point_cloud)
+    kmeans.visualize_clusters_skin_noskin()
+        
+        
+
+# finalLloyd = lloyd()
+macqueen()
+# print(len(finalLloyd[0]))
+# for i in range(len(finalLloyd[0])):
+#     for j in range(len(finalLloyd[0])):
+#         print(finalLloyd[0][i][j], finalMacqueen[i][j])
--- update_strategies.py
+++ update_strategies.py
@@ -25,7 +25,7 @@ class LloydUpdate(AbstractUpdate):
         iter = 0
         modified = True
         while modified:
-            print('\nIteration: {}'.format(iter))
+            # print('\nIteration: {}'.format(iter))
             iter += 1
 
             #Calculate new cluster
@@ -49,13 +49,13 @@ class LloydUpdate(AbstractUpdate):
                 for point_idx in clusters[key][1]:
                     avg += point_cloud[point_idx]
                 avg = (1/len(clusters[key][1]))*avg
-                print(avg)
+                # print(avg)
                 diff_centroids.append(clusters[key][0] - avg)
                 clusters[key][0] = avg
 
             #Check for abort condition:
             eps = 10**-6
-            print('Centroids moved by: {}'.format(diff_centroids))
+            # print('Centroids moved by: {}'.format(diff_centroids))
             modified = False
             for centroid in diff_centroids:
                 for elem in centroid:
@@ -79,33 +79,29 @@ class MacQueenUpdate(AbstractUpdate):
         idx_of_last_change = -1
         meaningful_diff = True
         prevClusters = {}
-        updated_centroid_indices = centroid_indices
+        updated_centroids = [point_cloud[i] for i in centroid_indices]
         mean_dist_per_iteration = {}
         while True:
             # Base conditions
-            if(n_iterations >= 5):
+            if(n_iterations >= 10):
                 break
             if not meaningful_diff:
                 break
 
-            print('\n\nIteration: {}'.format(n_iterations))
-            if n_iterations > 0:
-                updated_centroid_indices = [v['centroid'] for k,v in prevClusters.items()]
-
+            n_updates_per_iteration = 0
+            iter_start = time.time()
             tmpClusters = {}
-            for i,v in enumerate(updated_centroid_indices):
+            for i,v in enumerate(updated_centroids):
                 # Check if there has been a meaningful change in data since the last pass
-                if idx_of_last_change - i == 0:
-                    meaningful_diff = False
-                    break
                 update_start = time.time()
                 tmpClusters[i] = {
-                    'centroid': point_cloud[v],
+                    'centroid': v,
                     'point_indices': {},
-                    'sumOfPoints': point_cloud[v]
+                    'sumOfPoints': v
                 }
 
             for idx, point in enumerate(point_cloud):
+                
                 min_dist = float("inf")
                 dist = -1
                 cluster = -1
@@ -114,53 +110,26 @@ class MacQueenUpdate(AbstractUpdate):
                     if min_dist > dist:
                         min_dist = dist
                         cluster = clusterIdx
-                clusterData = tmpClusters[cluster]
-                
+                # print(min_dist, cluster, dist)
+                try:
+                    clusterData = tmpClusters[cluster]
+                except:
+                    print('Key error for: ', n_iterations, idx)
                 clusterData['point_indices'][idx] = dist
                 
                 newSum = np.sum([clusterData['sumOfPoints'], point], axis=0)
                 newCentroid = np.divide(newSum, len(clusterData['point_indices']))
-                # print('CLUSTER: {}: dist: {}, point index: {}, old centroid: {}, '\
-                #     'new sum {}, new centroid: {}'
-                #     .format(cluster, dist, idx, clusterData['centroid'], newSum, newCentroid))
-                
+
                 clusterData.update({'sumOfPoints': newSum})
                 if not np.allclose(newCentroid, clusterData['centroid'], epsilon):
                     clusterData.update({'centroid': newCentroid}) 
-                    n_updates += 1
+                    n_updates_per_iteration += 1
                     meaningful_diff = True
+                    idx_of_last_change = i
 
                 update_end = time.time()
                 sum_update_time += update_end - update_start
-                # print('Cluster updated')
-                # for cKey, cValue in clusterData.items():
-                #     print('Cluster Key: ', cKey, 'Values: ', cValue, '\n')
-                
-                
-                  
-            # #Reposition Centroids
-            # diff_centroids = []
-            # for key in clusters.keys():
-            #     avg = [0]*len(point_cloud[0])
-            #     for point_idx in clusters[key][1]:
-            #         avg += point_cloud[point_idx]
-            #     avg = (1/len(clusters[key][1]))*avg
-            #     print(avg)
-            #     diff_centroids.append(clusters[key][0] - avg)
-            #     clusters[key][0] = avg
 
-            #             #Check for abort condition:
-            # print('Centroids moved by: {}'.format(diff_centroids))
-            # modified = False
-            # for eps in epsilon:
-            #     for centroid in diff_centroids:
-            #         for elem in centroid:
-            #             if eps < elem:
-            #                 modified = True
-            #                 break
-            #         if modified: break
-            #     if modified: break
-            n_iterations += 1
             mean_dist_per_iteration[n_iterations] = {}
             for k,v in tmpClusters.items():
                 #Calculate the averages to see how well the run did
@@ -168,8 +137,22 @@ class MacQueenUpdate(AbstractUpdate):
                         'avg_dist' : v['centroid'],
                         'mean_of_points': np.mean(v['centroid'])
                 }
+            updated_centroids = [v['centroid'] for k,v in tmpClusters.items()]
+            if n_iterations > 1:
+                try:
+                    finishedList = [False for k,v in tmpClusters.items() if not np.allclose(prevClusters[k]['centroid'], tmpClusters[k]['centroid'], epsilon)]
+                    if False not in finishedList:
+                        meaningful_diff = False          
+                except:
+                    print('key error', prevClusters.keys(), tmpClusters.keys())
             prevClusters = tmpClusters
-            break
+            iter_end = time.time()
+            print('\nIteration: {}, time {}, updates: {}'
+                .format(n_iterations, iter_end - iter_start, n_updates_per_iteration))
+            n_updates += n_updates_per_iteration
+            n_iterations += 1
+
+
         end = time.time()
         #Meta data for the 
         print('Macqueens completed in {:.2f} seconds'.format(end - start))
@@ -191,4 +174,8 @@ class MacQueenUpdate(AbstractUpdate):
             for subK, subV in v.items():
                 print('cluster:', subK)
                 print('mean of centroid points', subV['mean_of_points'])
-        return tmpClusters
+        
+        finalPoints = {}
+        for k,v in prevClusters.items():
+            finalPoints[k] = [v['centroid'], v['point_indices'].keys()]
+        return finalPoints
